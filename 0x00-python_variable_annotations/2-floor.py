a0616eb343acd2f697ccce4670d50b246e8d6756
Blxee <aminedev57@gmail.com>
2024-01-11 11:16:25 +0100

    Added: task (1. Basic annotations - floor) of 0x00. Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""2. Basic annotations - floor"""
+
+
+def floor(n: float) -> int:
+    """Converts float to integer.
+
+    Args:
+        n (float): a float
+
+    Returns:
+        int: the floor of n
+    """
+    return int(n)
