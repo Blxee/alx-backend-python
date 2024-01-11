e0b7e63d313adfd00eac591d5b47581796d70674
Blxee <aminedev57@gmail.com>
2024-01-11 11:12:28 +0100

    Added: task (1. Basic annotations - concat) of 0x00. Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""1. Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Adds two numbers
+
+    Args:
+        st1 (str): first string.
+        st2 (str): second string.
+
+    Returns:
+        str: the concatenation of both strings.
+    """
+    return str1 + str2
