777acbb52f0792161ae61c35bb46a0afe2d05a01
Blxee <aminedev57@gmail.com>
2024-01-11 11:07:33 +0100

    Added: task (0. Basic annotations - add) of 0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,4 +3,13 @@
 
 
 def add(a: float, b: float) -> float:
+    """Adds two numbers
+
+    Args:
+        a (float): first operand.
+        b (float): second operand.
+
+    Returns:
+        float: the sum of a and b.
+    """
     return a + b
