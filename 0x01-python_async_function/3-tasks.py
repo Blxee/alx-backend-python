fab03f14a28647f973eb3c5f8c61ed1a611e798c
Blxee <aminedev57@gmail.com>
2024-01-16 00:54:09 +0100

    Added: task (3. Tasks) of 0x01. Python Async

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 """3. Tasks"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Creates a task from wait_random coroutine.
+
+    Args:
+        max_delay (int): maximun delay amount (in seconds).
+    Returns:
+        asyncio.Task: the newly created task object.
+    """
+    return asyncio.create_task(wait_random(max_delay))
