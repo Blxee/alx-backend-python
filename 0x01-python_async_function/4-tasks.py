99b2099c8e62eea6372d4d67cd5c8ac81b52fa1d
Blxee <aminedev57@gmail.com>
2024-01-16 00:58:49 +0100

    Added: task (4. Tasks) of 0x01. Python Async

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 """4. Tasks"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """spawns wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): amount of times to spawn wait_random.
+        max_delay (int): maximun delay amount (in seconds).
+    Returns:
+        list[float]: list of all the delays.
+    """
+    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    return sorted(await asyncio.gather(*coroutines))
