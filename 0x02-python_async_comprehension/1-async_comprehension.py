469556781aadb73f8d685966cca69fb7cfa6b9dd
Blxee <aminedev57@gmail.com>
2024-01-16 15:35:18 +0100

    Added: task (1. Async Comprehensions) of 0x02. Python - Async Comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """1. Async Comprehensions"""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Returns a list of 1 random number floats in range 0-10.
+
+    Returns:
+        list[float]: the generated list of floats.
+    """
+    return [i async for i in async_generator()]
