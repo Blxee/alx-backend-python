1001e9de1a69f878c11a45d91163e70dde8eaea8
Blxee <aminedev57@gmail.com>
2024-02-06 02:15:39 +0100

    Added: task (9. Integration tests) of 0x03. Unittests and Integration Tests

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module for testing functions in client.py module"""
+from client import GithubOrgClient
+from parameterized import parameterized
+import unittest
+from unittest.mock import patch
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test suit for GithubOrgClient class"""
+
+    @parameterized.expand([
+        ('google'),
+        ('abc'),
+    ])
+    def test_org(self, org):
+        client = GithubOrgClient(org)
+        with patch('utils.get_json') as mock_get_json:
+            mock_get_json.assert_called_once()
+
+    def test_public_repos(self):
+        pass
+
+    def test_public_repos_with_license(self):
+        pass
